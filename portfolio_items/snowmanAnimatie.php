b0853f63c55ee2403d5864ea5aaa29372e3e0bf5
Esmeralda <34519178+Esmoves@users.noreply.github.com>
2018-01-03 22:49:05 +0100

    Add files via upload
    js animation

---
+++ portfolio_items/snowmanAnimatie.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl-NL">
+	<header>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../include/style.css">
+		<script src="../include/javascript_snowman.js"></script>
+	</header>	
+	
+	<body>
+
+ 	<h3>The Snowman!</h3>
+	      
+        <div id="animatie">
+		    <canvas id="snowman" width="900" height="410"> </canvas> <!-- voor de sneeuwman -->
+		 </div>
+  	 </body>
+</html>
